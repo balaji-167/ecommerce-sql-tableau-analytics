0d93cd72509ee6a0ffc4bd67fed67a603578468b
Balaji Reddy <balaji.k1607@gmail.com>
2026-05-30 02:06:22 +0530

    Create sales_kpi.sql

---
+++ sql/gold_analytics_views/sales_kpi.sql
@@ -0,0 +1,24 @@
+/*
+
+This script builds a reusable view (gold.sales_kpis) to act as a single source of truth for our core business performance metrics.
+It aggregates transaction data from gold.fact_orders into a clean, presentation-ready layer for dashboards and stakeholder reporting.
+
+*/
+
+CREATE OR REPLACE VIEW gold.sales_kpis AS
+
+SELECT 
+	COUNT(DISTINCT orders_id) AS total_orders,
+	COUNT(DISTINCT customer_id) AS total_customers,
+	ROUND(SUM(order_total_usd)::NUMERIC, 2) AS total_revenue,
+	ROUND(AVG(order_total_usd)::NUMERIC, 2) AS average_order_value,
+	ROUND(
+		PERCENTILE_CONT(0.5)
+		WITHIN GROUP (ORDER BY order_total_usd)::NUMERIC,
+		2
+	) AS median_order_value,
+	ROUND(
+		SUM(order_total_usd)::NUMERIC / COUNT(DISTINCT customer_id),
+		2
+	) AS revenue_per_customer
+FROM gold.fact_orders;
